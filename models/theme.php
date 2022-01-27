43a6d012c196e23673b9a63d36f144930b4a58ae
akli hiba <ih_akli@esi.dz>
2022-01-28 00:35:00 +0100

    i stoped at the model functions for the theme.

---
+++ models/theme.php
@@ -0,0 +1,10 @@
+<?php
+    class Theme extends Model{
+        public function __construct($theme, $bg, $primary, $secondary, $border)
+        {
+            //change the colors
+            $sql = "UPDATE couleur"
+        }
+    }
+
+?>
